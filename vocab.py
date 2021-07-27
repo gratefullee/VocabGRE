3caeba18431490cbb9abe527f7e371d590f97339
Grace Lee <hyorim.lee.skorea@gmail.com>
2021-07-27 10:44:01 -0400

    fixed

--- vocab.py
+++ vocab.py
@@ -10,8 +10,8 @@ from pyfiglet import figlet_format
 
 os.system('clear')
 
-cprint(figlet_format('GRE Vocab!', font='starwars'),
-       'yellow', 'on_red', attrs=['bold'])
+# cprint(figlet_format('GRE Vocab!', font='starwars'),
+#        'yellow', 'on_red', attrs=['bold'])
 
 
 # Convert PDF to String
@@ -48,36 +48,48 @@ meanings = meanings[1:]
 vocab = {}
 for i,v in zip(words,meanings):
     vocab[i] = v
-    
-try: 
-    first = int(input('Made by Grace, hleeabc@gmail.com\nWords are referred from Manhattan1000 GRE Vocab.\nInput the index of the first word. (1-995): '))-1
-except:
-    first = int(input('Made by Grace, hleeabc@gmail.com\nWords are referred from Manhattan1000 GRE Vocab.\nInput the index of the first word. (1-995): '))-1
 
-try: 
-    last = int(input('Input the index of the last word. (1-995). This number should be larger than the previous number: '))-1
-except: 
-    last = int(input('Input the index of the last word. (1-995). This number should be larger than the previous number: '))-1
-    
-
-learn_quiz = int(input('Input 1 for learning, 2 for spelling test: '))
-
-# Lists to a dictionary
-vocab = {}
-for i,v in zip(words[first:last+1],meanings[first:last+1]):
-    vocab[i] = v
+# # Lists to a dictionary
+# vocab = {}
+# for i,v in zip(words[first:last+1],meanings[first:last+1]):
+#     vocab[i] = v
 
 def OneByOne():
-    global vocab
+    os.system('clear')
+
+    cprint(figlet_format('GRE Vocab!', font='starwars'),
+       'yellow', 'on_red', attrs=['bold'])
+
+
+#     global vocab
+    try: 
+        first = int(input('Made by Grace, hleeabc@gmail.com\nWords are referred from Manhattan1000 GRE Vocab.\nInput the index of the first word. (1-995): '))-1
+    except:
+        first = int(input('Made by Grace, hleeabc@gmail.com\nWords are referred from Manhattan1000 GRE Vocab.\nInput the index of the first word. (1-995): '))-1
+
+    try: 
+        last = int(input('Input the index of the last word. (1-995). This number should be larger than the previous number: '))-1
+    except: 
+        last = int(input('Input the index of the last word. (1-995). This number should be larger than the previous number: '))-1
+        
+    # Lists to a dictionary
+    vocab = {}
+    for i,v in zip(words[first:last+1],meanings[first:last+1]):
+        vocab[i] = v
+    
+
+    learn_quiz = int(input('Input 1 for learning, 2 for spelling test: '))
     if learn_quiz == 1:
         shuffle = int(input('Input 1 for shuffle, 2 for no shuffle: '))
+        os.system('clear')
         if shuffle == 2:
             index =1
             for i,v in vocab.items():
                 print ('\n'+str(index) +' out of '+ str(last-first+1)) 
                 print (i+': '+v)
-                next = int(input('\n'+'Input any letter for the next word: '))
+                next = input('\n'+'Input any letter for the next word: ')
                 index += 1
+                os.system('clear')
             print('Great work!')
         elif shuffle == 1:
             index = 1
@@ -86,12 +98,14 @@ def OneByOne():
             for i,v in vocab.items():
                 print ('\n'+str(index) +' out of '+ str(last-first+1)) 
                 print (i+': '+v)
-                next = int(input('\n'+'Input any letter for the next word: '))
+                next = input('\n'+'Input any letter for the next word: ')
                 index += 1
+                os.system('clear')
             print('Great work!')
         else:
             OneByOne()
     elif learn_quiz == 2:
+        os.system('clear')
         index = 1
         d_set = list(set(vocab))
         vocab = {i:vocab[i] for i in d_set}
@@ -104,9 +118,7 @@ def OneByOne():
                 else:
                     print ('Answer: '+i)
 
-        print ("Great job!")
-        
-        
+
         
         
     else:
